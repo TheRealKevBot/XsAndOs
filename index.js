6f30e39fa0443fae6b232943cab2a80e20f6304b
TheRealKevBot <Kglas001@gmail.com>
2021-06-09 12:24:56 -0600

    First

---
+++ index.js
@@ -0,0 +1,13 @@
+//  Check to see if a string has the same amount of 'x's and 'o's. 
+//  The method must return a boolean and be case insensitive. 
+//  The string can contain any char.
+
+function XO(str) {
+
+}
+
+console.log(XO('xo'));
+console.log(XO("xxOo"));
+console.log(XO("xxxm"));
+console.log(XO("Oo"));
+console.log(XO("ooom"));
